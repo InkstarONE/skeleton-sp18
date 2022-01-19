757f64451d231fd8c10e8c46fe74d3ae555f10be
zhanghao244 <zhanghao244@pinan.com.cn>
2022-01-19 10:23:59 +0800

    lab3

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class FlikTest {
+    @Test
+    public void testIsSameNumber(){
+        assertTrue(Flik.isSameNumber(128,128));
+    }
+}
